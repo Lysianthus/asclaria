d5c848e0e664b54c372e58f49ee478434addf83e
Lysianthus <clydelius@gmail.com>
2016-04-29 17:19:47 +0800

    Update .gitignore

---
+++ src/assets/js/statcounter.js
@@ -0,0 +1,5 @@
+var sc_project = 10151966;
+var sc_invisible = 1;
+var sc_security = "cfbc62ec";
+var scJsHost = (("https:" == document.location.protocol) ? "https://secure." : "http://www.");
+document.write("<sc" + "ript type='text/javascript' src='" + scJsHost + "statcounter.com/counter/counter.js'></" + "script>");
