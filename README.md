e1ca39c93b4df9f37263214755f9201148e220e5
Lysianthus <clydelius@gmail.com>
2016-05-04 12:45:30 +0800

    Add projects to Archived Projects list

--- README.md
+++ README.md
@@ -22,7 +22,7 @@ Enable Apache `rewrite_module`.
 * npm 3.3+
 * Sass 3.4+
 
-## Compilation
+## Asset Compilation
 
 1. Convert Sass files into CSS files. (This is usually done while editing to see changes live on the browser.)
 2. Compile via [Packager](https://github.com/Lysianthus/packager). Files will be located in the `dist` folder.
--- public/projects_archived.php
+++ public/projects_archived.php
@@ -11,6 +11,14 @@
 				<h2 class="project-title"><span>Natura</span></h2>
 				<p>Natura is a geography blog. This was a response to a blog requirement in Lysianthus's Geography 1 class. This blog is now archived.</p>
 			</div> <!-- .project -->
+			<div class="project">
+				<h2 class="project-title"><span>QT</span></h2>
+				<p>QT is a private URL shortener.</p>
+			</div> <!-- .project -->
+			<div class="project">
+				<h2 class="project-title"><span>Here with You</span></h2>
+				<p>Here with You is a fanlisting for the relationship between Keisaku Satou and Margery Daw from the <cite>Shakugan no Shana</cite> franchise.</p>
+			</div> <!-- .project -->
 		</div> <!-- .projects -->
 	</div> <!-- #content -->
 
