411b750996c1097010a373f548937a029314eeac
Lysianthus <ac@clydl.xyz>
2016-05-11 16:19:48 +0800

    Force HTTPS and add Clydl to Personal Projects

--- public/projects_personal.php
+++ public/projects_personal.php
@@ -10,17 +10,22 @@
 			<div class="project">
 				<h2 class="project-title"><span>Affelius</span></h2>
 				<p>Affelius is a creative website owned and managed by Lysianthus. This is where she archives, publishes, and showcases her creative works, such as written articles, illustrations, attempts in color adjustment and image manipulation, personally-taken photographs, design concepts, website templates, simple to complex web programs, online experiments, and many other original creations. Simply put, Affelius is Lysianthus’s creative repository.</p>
-				<p><a class="go" target="_blank" href="http://affeli.us"><img alt="" src="/assets/images/friendly_cat.png" /></a></p>
+				<p><a class="go" target="_blank" href="https://affeli.us"><img alt="" src="/assets/images/friendly_cat.png" /></a></p>
 			</div> <!-- .project -->
 			<div class="project">
 				<h2 class="project-title"><span>Asclaria</span></h2>
 				<p>Asclaria is Lysianthus’s umbrella network. All of her personal websites, cliques, fanlistings, fansites, shrines, tributes, and miscellaneous projects are under this umbrella.</p>
-				<p><a class="go" target="_blank" href="http://asclaria.org"><img alt="" src="/assets/images/friendly_cat.png" /></a></p>
+				<p><a class="go" target="_blank" href="https://asclaria.org"><img alt="" src="/assets/images/friendly_cat.png" /></a></p>
 			</div> <!-- .project -->
 			<div class="project">
 				<h2 class="project-title"><span>Clydoll</span></h2>
 				<p>Clydoll is Lysianthus’s personal homepage. This is her “home base”. Her accounts to other websites as well as her contact information can be found here.</p>
-				<p><a class="go" target="_blank" href="http://clydoll.me"><img alt="" src="/assets/images/friendly_cat.png" /></a></p>
+				<p><a class="go" target="_blank" href="https://clydoll.me"><img alt="" src="/assets/images/friendly_cat.png" /></a></p>
+			</div> <!-- .project -->
+			<div class="project">
+				<h2 class="project-title"><span>Clydl</span></h2>
+				<p>Clydl is a web development portfolio.</p>
+				<p><a class="go" target="_blank" href="https://clydl.xyz"><img alt="" src="/assets/images/friendly_cat.png" /></a></p>
 			</div> <!-- .project -->
 			<div class="project">
 				<h2 class="project-title"><span>Lumina</span></h2>
