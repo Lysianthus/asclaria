41d90407e0cb6e41795281a5e9b0151c0264e603
Lysianthus <she@lysianth.us>
2017-04-18 22:32:12 +0800

    :sparkles: Update linkage.php to show buttons instead of text links

--- public/linkage.php
+++ public/linkage.php
@@ -8,48 +8,43 @@
 		<article class="article">
 			<h2><span>Admired</span></h2>
 			<p>Here is a list of websites and networks that I admire! These networks give me so much inspiration. Do check them out!</p>
-			<ul>
-				<li><a target="_blank" href="http://aminuet.com">Aminuet</a></li>
-				<li><a target="_blank" href="http://arose.nu">Arose</a></li>
-				<li><a target="_blank" href="http://azurelight.net">Azure Light</a></li>
-				<li><a target="_blank" href="http://besaid.org">Besaid</a></li>
-				<li><a target="_blank" href="http://celestialis.com">Celestialis</a></li>
-				<li><a target="_blank" href="http://cuite.org">Cuite</a></li>
-				<li><a target="_blank" href="http://erzadel.net">Erzadel</a></li>
-				<li><a target="_blank" href="http://eternal-wings.net">Eternal Wings</a></li>
-				<li><a target="_blank" href="http://ethelion.net">Ethelion</a></li>
-				<li><a target="_blank" href="http://ishiryoku.net">Ishiryoku</a></li>
-				<li><a target="_blank" href="http://kyou.nu">Kyou</a></li>
-				<li><a target="_blank" href="http://lenne.nu">Lenne</a></li>
-				<li><a target="_blank" href="http://lets-volt.in">Let's Volt IN</a></li>
-				<li><a target="_blank" href="http://like-knives.org">Like-Knives</a></li>
-				<li><a target="_blank" href="http://londonboy.org">Londonboy</a></li>
-				<li><a target="_blank" href="http://mazohyst.org">Mazohyst</a></li>
-				<li><a target="_blank" href="http://nightbringer.net">Nightbringer</a></li>
-				<li><a target="_blank" href="http://ohmydarling.org">Oh My Darling</a></li>
-				<li><a target="_blank" href="http://overskill.org">Overskill</a></li>
-				<li><a target="_blank" href="http://papermug.net">Paper Mug</a></li>
-				<li><a target="_blank" href="http://paperscraps.ca">Paper Scraps</a></li>
-				<li><a target="_blank" href="http://piratesboard.net">Pirates Board</a></li>
-				<li><a target="_blank" href="http://potion.nu">Potion</a></li>
-				<li><a target="_blank" href="http://www.psyche.nu">Psyche</a></li>
-				<li><a target="_blank" href="http://redcrown.net">Redcrown</a></li>
-				<li><a target="_blank" href="http://rigelatin.net">Rigelatin</a></li>
-				<li><a target="_blank" href="http://rokuro.nu">Rokuro</a></li>
-				<li><a target="_blank" href="http://rydia.nu">Rydia</a></li>
-				<li><a target="_blank" href="http://shinkirou.net">Shinkirou</a></li>
-				<li><a target="_blank" href="http://skycrown.org">Skycrown</a></li>
-				<li><a target="_blank" href="http://snow-heart.net">Snow Heart</a></li>
-				<li><a target="_blank" href="http://sohryu.nu">Sohryu</a></li>
-				<li><a target="_blank" href="http://somnus.nu">Somnus</a></li>
-				<li><a target="_blank" href="http://www.sun-cryst.org">Suncryst</a></li>
-				<li><a target="_blank" href="http://sutarodo.net">Sutarodo</a></li>
-				<li><a target="_blank" href="http://valiantknife.org">The Valiant Knife</a></li>
-				<li><a target="_blank" href="http://www.wild-seven.org">Wild Seven</a></li>
-				<li><a target="_blank" href="http://withinmyworld.org">Within My World</a></li>
-				<li><a target="_blank" href="http://zabimaru.net">Zabimaru</a></li>
-				<li><a target="_blank" href="http://zeruda.org">Zeruda</a></li>
-			</ul>
+			<p style="display: flex; justify-content: center; align-items: flex-start; flex-wrap: wrap;">
+				<a target="_blank" href="http://aminuet.com" style="margin: 5px;"><img alt="" src="/assets/images/aminuet.gif" /></a>
+				<a target="_blank" href="http://azurelight.net" style="margin: 5px;"><img alt="" src="/assets/images/azurelight.gif" /></a>
+				<a target="_blank" href="http://besaid.org" style="margin: 5px;"><img alt="" src="/assets/images/besaid.png" /></a>
+				<a target="_blank" href="http://celestialis.com" style="margin: 5px;"><img alt="" src="/assets/images/celestialis.gif" /></a>
+				<a target="_blank" href="http://cuite.org" style="margin: 5px;"><img alt="" src="/assets/images/cuite.png" /></a>
+				<a target="_blank" href="http://eternal-wings.net" style="margin: 5px;"><img alt="" src="/assets/images/eternal-wings.png" /></a>
+				<a target="_blank" href="http://ishiryoku.net" style="margin: 5px;"><img alt="" src="/assets/images/ishiryoku.png?v=1" /></a>
+				<a target="_blank" href="http://kyou.nu" style="margin: 5px;"><img alt="" src="/assets/images/kyou.jpg" /></a>
+				<a target="_blank" href="http://like-knives.org" style="margin: 5px;"><img alt="" src="/assets/images/like-knives.png" /></a>
+				<a target="_blank" href="http://londonboy.org" style="margin: 5px;"><img alt="" src="/assets/images/londonboy.png" /></a>
+				<a target="_blank" href="http://nightbringer.net" style="margin: 5px;"><img alt="" src="/assets/images/nightbringer.gif" /></a>
+				<a target="_blank" href="http://ohmydarling.org" style="margin: 5px;"><img alt="" src="/assets/images/ohmydarling.jpg" /></a>
+				<a target="_blank" href="http://overskill.org" style="margin: 5px;"><img alt="" src="/assets/images/overskill.png" /></a>
+				<a target="_blank" href="http://piratesboard.net" style="margin: 5px;"><img alt="" src="/assets/images/piratesboard.png" /></a>
+				<a target="_blank" href="http://potion.nu" style="margin: 5px;"><img alt="" src="/assets/images/potion.png" /></a>
+				<a target="_blank" href="http://www.psyche.nu" style="margin: 5px;"><img alt="" src="/assets/images/psyche.jpg" /></a>
+				<a target="_blank" href="http://redcrown.net" style="margin: 5px;"><img alt="" src="/assets/images/redcrown.png?v=1" /></a>
+				<a target="_blank" href="http://rydia.nu" style="margin: 5px;"><img alt="" src="/assets/images/rydia.png" /></a>
+				<a target="_blank" href="http://shinkirou.net" style="margin: 5px;"><img alt="" src="/assets/images/shinkirou.png" /></a>
+				<a target="_blank" href="http://skycrown.org" style="margin: 5px;"><img alt="" src="/assets/images/skycrown.png" /></a>
+				<a target="_blank" href="http://snow-heart.net" style="margin: 5px;"><img alt="" src="/assets/images/snowheart.jpg" /></a>
+				<a target="_blank" href="http://somnus.nu" style="margin: 5px;"><img alt="" src="/assets/images/somnus.png" /></a>
+				<a target="_blank" href="http://www.sun-cryst.org" style="margin: 5px;"><img alt="" src="/assets/images/sun-cryst.gif" /></a>
+				<a target="_blank" href="http://sutarodo.net" style="margin: 5px;"><img alt="" src="/assets/images/sutarodo.png" /></a>
+				<a target="_blank" href="http://valiantknife.org" style="margin: 5px;"><img alt="" src="/assets/images/valiantknife.gif" /></a>
+				<a target="_blank" href="http://www.wild-seven.org" style="margin: 5px;"><img alt="" src="/assets/images/wild-seven.gif" /></a>
+				<a target="_blank" href="http://zabimaru.net" style="margin: 5px;"><img alt="" src="/assets/images/zabimaru.gif" /></a>
+				<a target="_blank" href="http://zeruda.org" style="margin: 5px;"><img alt="" src="/assets/images/zeruda.gif?v=1" /></a>
+			</p>
+			<p style="display: flex; justify-content: space-around; align-items: flex-start; flex-wrap: wrap;">
+				<a target="_blank" href="http://arose.nu">Arose</a>
+				<a target="_blank" href="http://lenne.nu">Lenne</a>
+				<a target="_blank" href="http://rigelatin.net">Rigelatin</a>
+				<a target="_blank" href="http://sohryu.nu">Sohryu</a>
+				<a target="_blank" href="http://withinmyworld.org">Within My World</a>
+			</p>
 			<h2><span>Listed At</span></h2>
 			<p class="align-center">
 				<a target="_blank" href="http://smoothsailing.asclaria.org"><img alt="" src="/umbrella/smoothsailing/assets/images/microbutton.png"></a>
