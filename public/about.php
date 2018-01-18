965dcefcee54c5211f4d5bc5401d28075c3afe93
Lysianthus <she@lysianth.us>
2018-01-19 03:20:54 +0800

    :pencil2: Third-person to first-person POV

--- public/about.php
+++ public/about.php
@@ -7,14 +7,14 @@
 		<h1 class="heading">About</h1>
 		<article class="article">
 			<h2><span>Hello!</span></h2>
-			<p>Welcome to <i>Asclaria</i>, <a href="/about/lysianthus">Lysianthus</a>’s umbrella network. All of her personal websites, cliques, fanlistings, fansites, shrines, tributes, and miscellaneous projects are under this umbrella.</p>
+			<p>Welcome to <i>Asclaria</i>, my (<a href="/about/lysianthus">Lysianthus</a>’s) umbrella network. All of my personal websites, cliques, fanlistings, fansites, shrines, tributes, and miscellaneous projects are under this umbrella.</p>
 			<h2><span>Umbrella? What?</span></h2>
-			<p>Lysianthus decided to call Asclaria an &ldquo;umbrella&rdquo; network, because it does exactly what an umbrella does. It is a thing that &ldquo;includes or contains many different elements or parts&rdquo;. In this context, the elements referred to are Lysianthus’s various online projects.</p>
+			<p>I decided to call Asclaria an &ldquo;umbrella&rdquo; network, because it does exactly what an umbrella does. It is a thing that &ldquo;includes or contains many different elements or parts&rdquo;. In this context, the elements referred to are my various online projects.</p>
 			<h2><span>What’s in a name?</span></h2>
-			<p>The network name &ldquo;<i>asclaria</i>&rdquo; came from nothing; it is a random combination of Lysianthus’s favorite letters, sounds, and syllables from the English alphabet.</p>
+			<p>The network name &ldquo;<i>asclaria</i>&rdquo; came from nothing; it is a random combination of my favorite letters, sounds, and syllables from the English alphabet.</p>
 			<p>Asclaria didn’t start as asclaria, however. It had undergone many name revamps. This umbrella network was once called <i>Lyffae</i>, <i>Lyqei</i>, <i>Ephevatic</i>, <i>Lascuvia</i>, <i>Clyre</i>, <i>Clyris</i>, and <i>Clyrant</i>. It was the unending search for the original name that both looks and sounds good. <i>Asclaria</i> was chosen for its first launch.</p>
 			<h2><span>Domains</span></h2>
-			<p>Lysianthus totally runs a few domains under this umbrella. They are:</p>
+			<p>This network runs on a few domains. They are:</p>
 			<ul>
 				<li>affeli.us</li>
 				<li>asclaria.org</li>
@@ -29,7 +29,7 @@
 			<h2><span>Content Breakdown</span></h2>
 			<p>Ideally, Asclaria is...</p>
 			<ul>
-				<li>25% personal &mdash; websites related to Lysianthus</li>
+				<li>25% personal &mdash; websites directly related to me</li>
 				<li>25% fangirling &mdash; fanlistings, fansites, shrines, and tributes</li>
 				<li>25% interactive &mdash; cliques and communities</li>
 				<li>25% nonsense &mdash; miscellaneous</li>
