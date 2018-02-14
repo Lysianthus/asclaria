29e9802b5650915a204634f9cceaba909ce3b33c
Lysianthus <she@lysianth.us>
2018-02-14 20:51:17 +0800

    :ok_hand: CSS keyframes syntax correction

--- public/includes/header.php
+++ public/includes/header.php
@@ -28,7 +28,7 @@ $title .= "Asclaria &mdash; Lysianthus’s umbrella network";
 
 	<link rel="icon" type="image/png" href="/assets/images/square_face.png" />
 
-	<link rel="stylesheet" href="/assets/css/asclaria-b776b44f21.css" />
+	<link rel="stylesheet" href="/assets/css/asclaria-df7f2efa79.css" />
 </head>
 
 <body>
