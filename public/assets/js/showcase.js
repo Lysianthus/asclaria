d0a3950acb9525c39efc4f74935f3f67ed834792
Lysianthus <she@lysianth.us>
2018-03-29 00:05:28 +0800

    :wrench: Put data into data sets instead of functions

--- public/assets/js/showcase.js
+++ public/assets/js/showcase.js
@@ -1,191 +1,147 @@
-function showInfo(s_info) {
+/* data */
 
-	s_title = s_info[0];
-	s_description = s_info[1];
-	s_url = s_info[2];
-	s_image = s_info[3];
+var categories = [
+	'personal',
+	'fan',
+	'interactive',
+	'miscellaneous'
+];
 
-	document.getElementById('showcaseContentTitle').innerHTML = s_title;
-	document.getElementById('showcaseContentDescription').innerHTML = s_description;
+var websites = [
+	{
+		"id": 1,
+		"title": "Affelius",
+		"url": "https://affeli.us",
+		"description": "Affelius is my creative repository. This is where I archive, publish, and showcase my creative works, such as written articles, illustrations, attempts in color adjustment and image manipulation, personally-taken photographs, design concepts, website templates, simple to complex web programs, online experiments, and many other original creations.",
+		"image": "/assets/images/affelius.png",
+		"category": "1"
+	},
+	{
+		"id": 2,
+		"title": "Asclaria",
+		"url": "https://asclaria.org",
+		"description": "Asclaria is my umbrella network. All of my personal websites, cliques, fanlistings, fansites, shrines, tributes, and miscellaneous projects are under this umbrella.",
+		"image": "/assets/images/asclaria.png?v=5",
+		"category": "1"
+	},
+	{
+		"id": 3,
+		"title": "Lysianthus",
+		"url": "https://lysianth.us",
+		"description": "Lysianthus is my personal homepage. This is my “home base”. My accounts to other websites as well as my contact information can be found here.",
+		"image": "/assets/images/lysianthus.png",
+		"category": "1"
+	},
+	{
+		"id": 4,
+		"title": "Paper Carvings",
+		"url": "https://papercarvings.lysianth.us",
+		"description": "Lysianthus is a personal blog, subtitled “Introspection and a Character Study.”",
+		"image": "/assets/images/papercarvings.png",
+		"category": "1"
+	},
+	{
+		"id": 5,
+		"title": "Leprd",
+		"url": "https://leprd.space",
+		"description": "Leprd is a free web hosting service.",
+		"image": "/assets/images/leprd.png?v=0",
+		"category": "1"
+	},
+	{
+		"id": 6,
+		"title": "Power Couple",
+		"url": "https://superwonder.asclaria.org",
+		"description": "Power Couple is a TFL-approved fanlisting for the relationship between Clark Kent/Kal-El of Krypton (Superman) and Diana of Themyscira (Wonder Woman) of DC Comics.",
+		"image": "/assets/images/superwonder.png",
+		"category": "2"
+	},
+	{
+		"id": 7,
+		"title": "Superman/Wonder Woman Fan Fiction Index",
+		"url": "https://smwwfics.asclaria.org",
+		"description": "This is a comprehensive list of Superman &times; Wonder Woman fan fiction read and collected from all over the internet by various authors.",
+		"image": "/assets/images/smwwfics.png",
+		"category": "2"
+	},
+	{
+		"id": 8,
+		"title": "VERA FARMIGA EBOOKS",
+		"url": "https://twitter.com/VeraFarmigaBot",
+		"description": 'a progress in work, and a <a target="_blank" href="https://twitter.com/VeraFarmiga">@VeraFarmiga</a> bot',
+		"image": "/assets/images/verafarmigabot.png",
+		"category": "2"
+	},
+	{
+		"id": 9,
+		"title": "Verna Flamingo",
+		"url": "https://vernaflamingo.isamotherfucking.ninja",
+		"description": 'Verna Flamingo is a splash page dedicated to Vera Farmiga. This project is also categorized under <a target="_blank" href="http://th-hq.asclaria.org">Throwaway Projects</a>.',
+		"image": "/assets/images/vernaflamingo.png",
+		"category": "2"
+	},
+	{
+		"id": 10,
+		"title": "Smooth Sailing Listings",
+		"url": "https://smoothsailing.asclaria.org",
+		"description": "Smooth Sailing Listings is a web directory or listing for websites of any kind. It is an automated and safe way to list your website!",
+		"image": "/assets/images/smoothsailing.png",
+		"category": "3"
+	},
+	{
+		"id": 11,
+		"title": "Inner Buoyancy",
+		"url": "https://twitter.com/InnerBuoyancy",
+		"description": "Inner Buoyancy is a positivity bot that tweets reminders a couple of times a day.",
+		"image": "/assets/images/innerbuoyancy.png",
+		"category": "3"
+	},
+	{
+		"id": 12,
+		"title": "Tests",
+		"url": "https://tests.asclaria.org",
+		"description": "Tests is a subdomain for quizzes and tests for personal or external uses.",
+		"image": "/assets/images/tests.png",
+		"category": "3"
+	},
+	{
+		"id": 13,
+		"title": "Archives",
+		"url": "https://archives.asclaria.org",
+		"description": "Archives is the place for all things past. It serves as my personal mirror for some favorite things that no longer exist on the internet.",
+		"image": "/assets/images/archives.png",
+		"category": "4"
+	},
+	{
+		"id": 14,
+		"title": "VVVVVV",
+		"url": "https://vvvvvv.asclaria.org",
+		"description": "VVVVVV is a place of nonsense. Sometimes, I put temporary projects here. Most often, this is where I just mess around.",
+		"image": "/assets/images/vvvvvv.png",
+		"category": "4"
+	},
+	{
+		"id": 15,
+		"title": "Throwaway Headquarters",
+		"url": "https://th-hq.asclaria.org",
+		"description": "Throwaway Headquarters is the headquarters for all of my throwaway websites.",
+		"image": "/assets/images/th-hq.png",
+		"category": "4"
+	}
+];
+
+
+/* functions */
+
+function showInfo(w_id) {
+
+	w_id -= 1;
+
+	document.getElementById('showcaseContentTitle').innerHTML = websites[w_id].title;
+	document.getElementById('showcaseContentDescription').innerHTML = websites[w_id].description;
 	document.getElementById('showcaseContentUrl').setAttribute("target", "_blank");
-	document.getElementById('showcaseContentUrl').setAttribute("href", s_url);
+	document.getElementById('showcaseContentUrl').setAttribute("href", websites[w_id].url);
 	document.getElementById('showcaseContentUrl').innerHTML = 'Visit website &rarr;';
-	document.getElementById('showcaseContentImage').setAttribute("src", s_image);
+	document.getElementById('showcaseContentImage').setAttribute("src", websites[w_id].image);
 
-}
-
-function sAffelius() {
-	var s_title = "Affelius";
-	var s_description = "Affelius is my creative repository. This is where I archive, publish, and showcase my creative works, such as written articles, illustrations, attempts in color adjustment and image manipulation, personally-taken photographs, design concepts, website templates, simple to complex web programs, online experiments, and many other original creations.";
-	var s_url = 'https://affeli.us';
-	var s_image = '/assets/images/affelius.png';
-
-	var s_info = [s_title, s_description, s_url, s_image];
-
-	showInfo(s_info);
-}
-
-function sAsclaria() {
-	var s_title = "Asclaria";
-	var s_description = "Asclaria is my umbrella network. All of my personal websites, cliques, fanlistings, fansites, shrines, tributes, and miscellaneous projects are under this umbrella.";
-	var s_url = 'https://asclaria.org';
-	var s_image = '/assets/images/asclaria.png?v=5';
-
-	var s_info = [s_title, s_description, s_url, s_image];
-
-	showInfo(s_info);
-}
-
-function sLysianthus() {
-	var s_title = "Lysianthus";
-	var s_description = "Lysianthus is my personal homepage. This is my “home base”. My accounts to other websites as well as my contact information can be found here.";
-	var s_url = "https://lysianth.us";
-	var s_image = '/assets/images/lysianthus.png';
-
-	var s_info = [s_title, s_description, s_url, s_image];
-
-	showInfo(s_info);
-}
-
-function sPaperCarvings() {
-	var s_title = "Paper Carvings";
-	var s_description = "Lysianthus is a personal blog, subtitled “Introspection and a Character Study.”";
-	var s_url = "https://papercarvings.lysianth.us";
-	var s_image = '/assets/images/papercarvings.png';
-
-	var s_info = [s_title, s_description, s_url, s_image];
-
-	showInfo(s_info);
-}
-
-function sLeprd() {
-	var s_title = "Leprd";
-	var s_description = "Leprd is a free web hosting service.";
-	var s_url = "https://leprd.space";
-	var s_image = '/assets/images/leprd.png?v=0';
-
-	var s_info = [s_title, s_description, s_url, s_image];
-
-	showInfo(s_info);
-}
-
-function sPowerCouple() {
-	var s_title = "Power Couple";
-	var s_description = "Power Couple is a TFL-approved fanlisting for the relationship between Clark Kent/Kal-El of Krypton (Superman) and Diana of Themyscira (Wonder Woman) of DC Comics.";
-	var s_url = "https://superwonder.asclaria.org";
-	var s_image = '/assets/images/superwonder.png';
-
-	var s_info = [s_title, s_description, s_url, s_image];
-
-	showInfo(s_info);
-}
-
-function sSMWWFF() {
-	var s_title = "Superman/Wonder Woman Fan Fiction Index";
-	var s_description = "This is a comprehensive list of Superman &times; Wonder Woman fan fiction read and collected from all over the internet by various authors.";
-	var s_url = "https://smwwfics.asclaria.org";
-	var s_image = '/assets/images/smwwfics.png';
-
-	var s_info = [s_title, s_description, s_url, s_image];
-
-	showInfo(s_info);
-}
-
-function sVeraFarmigaEbooks() {
-	var s_title = "VERA FARMIGA EBOOKS";
-	var s_description = 'a progress in work, and a <a target="_blank" href="https://twitter.com/VeraFarmiga">@VeraFarmiga</a> bot';
-	var s_url = "https://twitter.com/VeraFarmigaBot";
-	var s_image = '/assets/images/verafarmigabot.png';
-
-	var s_info = [s_title, s_description, s_url, s_image];
-
-	showInfo(s_info);
-}
-
-function sVernaFlamingo() {
-	var s_title = "Verna Flamingo";
-	var s_description = 'Verna Flamingo is a splash page dedicated to Vera Farmiga. This project is also categorized under <a target="_blank" href="http://th-hq.asclaria.org">Throwaway Projects</a>.';
-	var s_url = "https://vernaflamingo.isamotherfucking.ninja";
-	var s_image = '/assets/images/vernaflamingo.png';
-
-	var s_info = [s_title, s_description, s_url, s_image];
-
-	showInfo(s_info);
-}
-
-function sSmoothSailing() {
-	var s_title = "Smooth Sailing Listings";
-	var s_description = "Smooth Sailing Listings is a web directory or listing for websites of any kind. It is an automated and safe way to list your website!";
-	var s_url = "https://smoothsailing.asclaria.org";
-	var s_image = '/assets/images/smoothsailing.png';
-
-	var s_info = [s_title, s_description, s_url, s_image];
-
-	showInfo(s_info);
-}
-
-function sInnerBuoyancy() {
-	var s_title = "Inner Buoyancy";
-	var s_description = "Inner Buoyancy is a positivity bot that tweets reminders a couple of times a day.";
-	var s_url = "https://twitter.com/InnerBuoyancy";
-	var s_image = '/assets/images/innerbuoyancy.png';
-
-	var s_info = [s_title, s_description, s_url, s_image];
-
-	showInfo(s_info);
-}
-
-function sTests() {
-	var s_title = "Tests";
-	var s_description = "Tests is a subdomain for quizzes and tests for personal or external uses.";
-	var s_url = "https://tests.asclaria.org";
-	var s_image = '/assets/images/tests.png';
-
-	var s_info = [s_title, s_description, s_url, s_image];
-
-	showInfo(s_info);
-}
-
-function sChocobear() {
-	var s_title = "Chocobear";
-	var s_description = "Chocobear is a temporary file hosting service.";
-	var s_url = "https://chocobear.site";
-	var s_image = '/assets/images/chocobear.png';
-
-	var s_info = [s_title, s_description, s_url, s_image];
-
-	showInfo(s_info);
-}
-
-function sArchives() {
-	var s_title = "Archives";
-	var s_description = "Archives is the place for all things past. It serves as my personal mirror for some favorite things that no longer exist on the internet.";
-	var s_url = "https://archives.asclaria.org";
-	var s_image = '/assets/images/archives.png';
-
-	var s_info = [s_title, s_description, s_url, s_image];
-
-	showInfo(s_info);
-}
-
-function sVVVVVV() {
-	var s_title = "VVVVVV";
-	var s_description = "VVVVVV is a place of nonsense. Sometimes, I put temporary projects here. Most often, this is where I just mess around.";
-	var s_url = "https://vvvvvv.asclaria.org";
-	var s_image = '/assets/images/vvvvvv.png';
-
-	var s_info = [s_title, s_description, s_url, s_image];
-
-	showInfo(s_info);
-}
-
-function sTHHQ() {
-	var s_title = "Throwaway Headquarters";
-	var s_description = "Throwaway Headquarters is the headquarters for all of my throwaway websites.";
-	var s_url = "https://th-hq.asclaria.org";
-	var s_image = '/assets/images/th-hq.png';
-
-	var s_info = [s_title, s_description, s_url, s_image];
-
-	showInfo(s_info);
 }
--- public/projects.php
+++ public/projects.php
@@ -40,35 +40,35 @@ include 'includes/header.php';
 
 			<nav class="showcase__header__menu">
 				<h2 class="showcase__header__menu__heading">&mdash; Personal Projects &mdash;</h2>
-				<a class="showcase_item" href="javascript:;" onclick="sAffelius();">
+				<a class="showcase_item" href="javascript:;" onclick="showInfo(1);">
 					<img class="showcase__item__thumbnail" alt="" src="/assets/images/square_face.png" />
 					<div class="showcase__item__info">
 						<p class="showcase__item__title">Affelius</p>
 						<p class="showcase__item__url">affeli.us</p>
 					</div> <!-- .showcase__item__info -->
 				</a> <!-- .showcase_item -->
-				<a class="showcase_item" href="javascript:;" onclick="sAsclaria();">
+				<a class="showcase_item" href="javascript:;" onclick="showInfo(2);">
 					<img class="showcase__item__thumbnail" alt="" src="/assets/images/square_hands.png" />
 					<div class="showcase__item__info">
 						<p class="showcase__item__title">Asclaria</p>
 						<p class="showcase__item__url">asclaria.org</p>
 					</div> <!-- .showcase__item__info -->
 				</a> <!-- .showcase_item -->
-				<a class="showcase_item" href="javascript:;" onclick="sLysianthus();">
+				<a class="showcase_item" href="javascript:;" onclick="showInfo(3);">
 					<img class="showcase__item__thumbnail" alt="" src="/assets/images/square_face.png" />
 					<div class="showcase__item__info">
 						<p class="showcase__item__title">Lysianthus</p>
 						<p class="showcase__item__url">lysianth.us</p>
 					</div> <!-- .showcase__item__info -->
 				</a> <!-- .showcase_item -->
-				<a class="showcase_item" href="javascript:;" onclick="sPaperCarvings();">
+				<a class="showcase_item" href="javascript:;" onclick="showInfo(4);">
 					<img class="showcase__item__thumbnail" alt="" src="/assets/images/square_hands.png" />
 					<div class="showcase__item__info">
 						<p class="showcase__item__title">Paper Carvings</p>
 						<p class="showcase__item__url">papercarvings.lysianth.us</p>
 					</div> <!-- .showcase__item__info -->
 				</a> <!-- .showcase_item -->
-				<a class="showcase_item" href="javascript:;" onclick="sLeprd();">
+				<a class="showcase_item" href="javascript:;" onclick="showInfo(5);">
 					<img class="showcase__item__thumbnail" alt="" src="/assets/images/square_face.png" />
 					<div class="showcase__item__info">
 						<p class="showcase__item__title">Leprd</p>
@@ -81,28 +81,28 @@ include 'includes/header.php';
 
 			<nav class="showcase__header__menu">
 				<h2 class="showcase__header__menu__heading">&mdash; Fan Projects &mdash;</h2>
-				<a class="showcase_item" href="javascript:;" onclick="sPowerCouple();">
+				<a class="showcase_item" href="javascript:;" onclick="showInfo(6);">
 					<img class="showcase__item__thumbnail" alt="" src="/assets/images/superwonder_thumb.png" />
 					<div class="showcase__item__info">
 						<p class="showcase__item__title">Power Couple</p>
 						<p class="showcase__item__url">superwonder.asclaria.org</p>
 					</div> <!-- .showcase__item__info -->
 				</a> <!-- .showcase_item -->
-				<a class="showcase_item" href="javascript:;" onclick="sSMWWFF();">
+				<a class="showcase_item" href="javascript:;" onclick="showInfo(7);">
 					<img class="showcase__item__thumbnail" alt="" src="/assets/images/smwwfics_thumb.png" />
 					<div class="showcase__item__info">
 						<p class="showcase__item__title">Superman/Wonder Woman Fan Fiction Index</p>
 						<p class="showcase__item__url">smwwfics.asclaria.org</p>
 					</div> <!-- .showcase__item__info -->
 				</a> <!-- .showcase_item -->
-				<a class="showcase_item" href="javascript:;" onclick="sVeraFarmigaEbooks();">
+				<a class="showcase_item" href="javascript:;" onclick="showInfo(8);">
 					<img class="showcase__item__thumbnail" alt="" src="/assets/images/verafarmigabot_thumb.png" />
 					<div class="showcase__item__info">
 						<p class="showcase__item__title">VERA FARMIGA EBOOKS</p>
 						<p class="showcase__item__url">twitter.com/verafarmigabot</p>
 					</div> <!-- .showcase__item__info -->
 				</a> <!-- .showcase_item -->
-				<a class="showcase_item" href="javascript:;" onclick="sVernaFlamingo();">
+				<a class="showcase_item" href="javascript:;" onclick="showInfo(9);">
 					<img class="showcase__item__thumbnail" alt="" src="/assets/images/vernaflamingo_thumb.png" />
 					<div class="showcase__item__info">
 						<p class="showcase__item__title">Verna Flamingo</p>
@@ -133,55 +133,48 @@ include 'includes/header.php';
 
 			<nav class="showcase__header__menu">
 				<h2 class="showcase__header__menu__heading">&mdash; Interactive Projects &mdash;</h2>
-				<a class="showcase_item" href="javascript:;" onclick="sSmoothSailing();">
+				<a class="showcase_item" href="javascript:;" onclick="showInfo(10);">
 					<img class="showcase__item__thumbnail" alt="" src="/assets/images/square_face.png" />
 					<div class="showcase__item__info">
 						<p class="showcase__item__title">Smooth Sailing Listings</p>
 						<p class="showcase__item__url">smoothsailing.asclaria.org</p>
 					</div> <!-- .showcase__item__info -->
 				</a> <!-- .showcase_item -->
-				<a class="showcase_item" href="javascript:;" onclick="sInnerBuoyancy();">
+				<a class="showcase_item" href="javascript:;" onclick="showInfo(11);">
 					<img class="showcase__item__thumbnail" alt="" src="/assets/images/square_hands.png" />
 					<div class="showcase__item__info">
 						<p class="showcase__item__title">Inner Buoyancy</p>
 						<p class="showcase__item__url">twitter.com/InnerBuoyancy</p>
 					</div> <!-- .showcase__item__info -->
 				</a> <!-- .showcase_item -->
-				<a class="showcase_item" href="javascript:;" onclick="sTests();">
+				<a class="showcase_item" href="javascript:;" onclick="showInfo(12);">
 					<img class="showcase__item__thumbnail" alt="" src="/assets/images/square_face.png" />
 					<div class="showcase__item__info">
 						<p class="showcase__item__title">Tests</p>
 						<p class="showcase__item__url">tests.asclaria.org</p>
 					</div> <!-- .showcase__item__info -->
 				</a> <!-- .showcase_item -->
-				<!--a class="showcase_item" href="javascript:;" onclick="sChocobear();">
-					<img class="showcase__item__thumbnail" alt="" src="/assets/images/square_hands.png" />
-					<div class="showcase__item__info">
-						<p class="showcase__item__title">Chocobear</p>
-						<p class="showcase__item__url">chocobear.site</p>
-					</div> <!-- .showcase__item__info -->
-				<!--/a> <!-- .showcase_item -->
 			</nav> <!-- .showcase__header__menu -->
 			
 			<?php elseif ($category == 'miscellaneous') : ?>
 
 			<nav class="showcase__header__menu">
 				<h2 class="showcase__header__menu__heading">&mdash; Miscellaneous Projects &mdash;</h2>
-				<a class="showcase_item" href="javascript:;" onclick="sArchives();">
+				<a class="showcase_item" href="javascript:;" onclick="showInfo(13);">
 					<img class="showcase__item__thumbnail" alt="" src="/assets/images/square_face.png" />
 					<div class="showcase__item__info">
 						<p class="showcase__item__title">Archives</p>
 						<p class="showcase__item__url">archives.asclaria.org</p>
 					</div> <!-- .showcase__item__info -->
 				</a> <!-- .showcase_item -->
-				<a class="showcase_item" href="javascript:;" onclick="sVVVVVV();">
+				<a class="showcase_item" href="javascript:;" onclick="showInfo(14);">
 					<img class="showcase__item__thumbnail" alt="" src="/assets/images/square_hands.png" />
 					<div class="showcase__item__info">
 						<p class="showcase__item__title">VVVVVV</p>
 						<p class="showcase__item__url">vvvvvv.asclaria.org</p>
 					</div> <!-- .showcase__item__info -->
 				</a> <!-- .showcase_item -->
-				<a class="showcase_item" href="javascript:;" onclick="sTHHQ();">
+				<a class="showcase_item" href="javascript:;" onclick="showInfo(15);">
 					<img class="showcase__item__thumbnail" alt="" src="/assets/images/square_face.png" />
 					<div class="showcase__item__info">
 						<p class="showcase__item__title">Throwaway Headquarters</p>
@@ -204,6 +197,6 @@ include 'includes/header.php';
 		</main> <!-- .showcase__content -->
 	</div> <!-- .showcase -->
 
-	<script src="/assets/js/showcase.js?v=5.0.3"></script>
+	<script src="/assets/js/showcase.js?v=5.1.0"></script>
 
 <?php include 'includes/footer.php'; ?>
