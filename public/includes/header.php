422cfc8bafbfd60e9b7ac7142b379acc969c4b30
Lysianthus <she@lysianth.us>
2018-04-18 18:44:36 +0800

    :lipstick: .showcase__item__url cosmetics

--- public/includes/header.php
+++ public/includes/header.php
@@ -28,7 +28,7 @@ $title .= "Asclaria &mdash; Lysianthus’s umbrella network";
 
 	<link rel="icon" type="image/png" href="/assets/images/square_face.png" />
 
-	<link rel="stylesheet" href="/assets/css/asclaria-d507674684.css" />
+	<link rel="stylesheet" href="/assets/css/asclaria-6bfc5641ac.css" />
 </head>
 
 <body>
