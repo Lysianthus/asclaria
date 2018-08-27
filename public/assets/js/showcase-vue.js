dd9373fd543cc54dddf6ba3d75bc3157ad5a27f5
Lysianthus <she@lysianth.us>
2018-08-28 01:05:21 +0800

    :art: Make websites.category an integer value

--- public/assets/js/showcase-vue.js
+++ public/assets/js/showcase-vue.js
@@ -15,7 +15,7 @@ var websites = [
 		"description": "Affelius is my creative repository. This is where I archive, publish, and showcase my creative works, such as written articles, illustrations, attempts in color adjustment and image manipulation, personally-taken photographs, design concepts, website templates, simple to complex web programs, online experiments, and many other original creations.",
 		"image": "/assets/images/affelius.png",
 		"thumbnail": "/assets/images/square_face.png",
-		"category": "1"
+		"category": 1
 	},
 	{
 		"id": 2,
@@ -24,7 +24,7 @@ var websites = [
 		"description": "Asclaria is my umbrella network. All of my personal websites, cliques, fanlistings, fansites, shrines, tributes, and miscellaneous projects are under this umbrella.",
 		"image": "/assets/images/asclaria.png?v=5",
 		"thumbnail": "/assets/images/square_hands.png",
-		"category": "1"
+		"category": 1
 	},
 	{
 		"id": 3,
@@ -33,7 +33,7 @@ var websites = [
 		"description": "Lysianthus is my personal homepage. This is my “home base”. My accounts to other websites as well as my contact information can be found here.",
 		"image": "/assets/images/lysianthus.png?v=1",
 		"thumbnail": "/assets/images/square_face.png",
-		"category": "1"
+		"category": 1
 	},
 	{
 		"id": 4,
@@ -42,7 +42,7 @@ var websites = [
 		"description": "Lysianthus is a personal blog, subtitled “Introspection and a Character Study.”",
 		"image": "/assets/images/papercarvings.png",
 		"thumbnail": "/assets/images/square_hands.png",
-		"category": "1"
+		"category": 1
 	},
 	{
 		"id": 5,
@@ -51,7 +51,7 @@ var websites = [
 		"description": "Leprd is a free web hosting service geared towards website hobbyists.",
 		"image": "/assets/images/leprd.png?v=0",
 		"thumbnail": "/assets/images/square_face.png",
-		"category": "1"
+		"category": 1
 	},
 	{
 		"id": 6,
@@ -60,7 +60,7 @@ var websites = [
 		"description": "Power Couple is a TFL-approved fanlisting for the relationship between Clark Kent/Kal-El of Krypton (Superman) and Diana of Themyscira (Wonder Woman) of DC Comics.",
 		"image": "/assets/images/superwonder.png",
 		"thumbnail": "/assets/images/superwonder_thumb.png",
-		"category": "2"
+		"category": 2
 	},
 	{
 		"id": 7,
@@ -69,7 +69,7 @@ var websites = [
 		"description": "This is a comprehensive list of Superman &times; Wonder Woman fan fiction read and collected from all over the internet by various authors.",
 		"image": "/assets/images/smwwfics.png?v=1",
 		"thumbnail": "/assets/images/smwwfics_thumb.png",
-		"category": "2"
+		"category": 2
 	},
 	{
 		"id": 8,
@@ -78,7 +78,7 @@ var websites = [
 		"description": "a Twitter bot that tweets at least one animated GIF of Balalaika from Black Lagoon per day",
 		"image": "/assets/images/motelhoscow.png",
 		"thumbnail": "/assets/images/motelhoscow_thumb.png",
-		"category": "2"
+		"category": 2
 	},
 	{
 		"id": 9,
@@ -87,7 +87,7 @@ var websites = [
 		"description": 'a progress in work, and a <a target="_blank" href="https://twitter.com/VeraFarmiga">@VeraFarmiga</a> bot, with words scrambled from her tweets',
 		"image": "/assets/images/verafarmigabot.png",
 		"thumbnail": "/assets/images/verafarmigabot_thumb.png",
-		"category": "2"
+		"category": 2
 	},
 	{
 		"id": 10,
@@ -96,7 +96,7 @@ var websites = [
 		"description": 'Verna Flamingo is a splash page dedicated to Vera Farmiga. This project is also categorized under <a target="_blank" href="http://th-hq.asclaria.org">Throwaway Projects</a>.',
 		"image": "/assets/images/vernaflamingo.png",
 		"thumbnail": "/assets/images/vernaflamingo_thumb.png",
-		"category": "2"
+		"category": 2
 	},
 	{
 		"id": 11,
@@ -105,7 +105,7 @@ var websites = [
 		"description": "Smooth Sailing Listings is a web directory or listing for websites of any kind. It is an automated and safe way to list your website!",
 		"image": "/assets/images/smoothsailing.png",
 		"thumbnail": "/assets/images/square_face.png",
-		"category": "3"
+		"category": 3
 	},
 	{
 		"id": 12,
@@ -114,7 +114,7 @@ var websites = [
 		"description": "Inner Buoyancy is a positivity bot that tweets reminders a couple of times a day.",
 		"image": "/assets/images/innerbuoyancy.png",
 		"thumbnail": "/assets/images/square_hands.png",
-		"category": "3"
+		"category": 3
 	},
 	{
 		"id": 13,
@@ -123,7 +123,7 @@ var websites = [
 		"description": "Cat FTP lets you store files temporarily for seven (7) days.",
 		"image": "/assets/images/cat.png",
 		"thumbnail": "/assets/images/square_face.png",
-		"category": "3"
+		"category": 3
 	},
 	{
 		"id": 14,
@@ -132,7 +132,7 @@ var websites = [
 		"description": "Tests is a subdomain for quizzes and tests for personal or external uses.",
 		"image": "/assets/images/tests.png",
 		"thumbnail": "/assets/images/square_hands.png",
-		"category": "3"
+		"category": 3
 	},
 	{
 		"id": 15,
@@ -141,7 +141,7 @@ var websites = [
 		"description": "Archives is the place for all things past. It serves as my personal mirror for some favorite things that no longer exist on the internet.",
 		"image": "/assets/images/archives.png",
 		"thumbnail": "/assets/images/square_face.png",
-		"category": "4"
+		"category": 4
 	},
 	{
 		"id": 16,
@@ -150,7 +150,7 @@ var websites = [
 		"description": "VVVVVV is a place of nonsense. Sometimes, I put temporary projects here. Most often, this is where I just mess around.",
 		"image": "/assets/images/vvvvvv.png",
 		"thumbnail": "/assets/images/square_hands.png",
-		"category": "4"
+		"category": 4
 	},
 	{
 		"id": 17,
@@ -159,7 +159,7 @@ var websites = [
 		"description": "Throwaway Headquarters is the headquarters for all of my throwaway websites.",
 		"image": "/assets/images/th-hq.png",
 		"thumbnail": "/assets/images/square_face.png",
-		"category": "4"
+		"category": 4
 	}
 ];
 
--- public/assets/js/showcase.js
+++ public/assets/js/showcase.js
@@ -14,7 +14,7 @@ var websites = [
 		"url": "https://affeli.us",
 		"description": "Affelius is my creative repository. This is where I archive, publish, and showcase my creative works, such as written articles, illustrations, attempts in color adjustment and image manipulation, personally-taken photographs, design concepts, website templates, simple to complex web programs, online experiments, and many other original creations.",
 		"image": "/assets/images/affelius.png",
-		"category": "1"
+		"category": 1
 	},
 	{
 		"id": 2,
@@ -22,7 +22,7 @@ var websites = [
 		"url": "https://asclaria.org",
 		"description": "Asclaria is my umbrella network. All of my personal websites, cliques, fanlistings, fansites, shrines, tributes, and miscellaneous projects are under this umbrella.",
 		"image": "/assets/images/asclaria.png?v=5",
-		"category": "1"
+		"category": 1
 	},
 	{
 		"id": 3,
@@ -30,7 +30,7 @@ var websites = [
 		"url": "https://lysianth.us",
 		"description": "Lysianthus is my personal homepage. This is my “home base”. My accounts to other websites as well as my contact information can be found here.",
 		"image": "/assets/images/lysianthus.png?v=1",
-		"category": "1"
+		"category": 1
 	},
 	{
 		"id": 4,
@@ -38,7 +38,7 @@ var websites = [
 		"url": "https://papercarvings.lysianth.us",
 		"description": "Lysianthus is a personal blog, subtitled “Introspection and a Character Study.”",
 		"image": "/assets/images/papercarvings.png",
-		"category": "1"
+		"category": 1
 	},
 	{
 		"id": 5,
@@ -46,7 +46,7 @@ var websites = [
 		"url": "https://leprd.space",
 		"description": "Leprd is a free web hosting service geared towards website hobbyists.",
 		"image": "/assets/images/leprd.png?v=0",
-		"category": "1"
+		"category": 1
 	},
 	{
 		"id": 6,
@@ -54,7 +54,7 @@ var websites = [
 		"url": "https://superwonder.asclaria.org",
 		"description": "Power Couple is a TFL-approved fanlisting for the relationship between Clark Kent/Kal-El of Krypton (Superman) and Diana of Themyscira (Wonder Woman) of DC Comics.",
 		"image": "/assets/images/superwonder.png",
-		"category": "2"
+		"category": 2
 	},
 	{
 		"id": 7,
@@ -62,7 +62,7 @@ var websites = [
 		"url": "https://smwwfics.asclaria.org",
 		"description": "This is a comprehensive list of Superman &times; Wonder Woman fan fiction read and collected from all over the internet by various authors.",
 		"image": "/assets/images/smwwfics.png?v=1",
-		"category": "2"
+		"category": 2
 	},
 	{
 		"id": 8,
@@ -70,7 +70,7 @@ var websites = [
 		"url": "https://twitter.com/VeraFarmigaBot",
 		"description": 'a progress in work, and a <a target="_blank" href="https://twitter.com/VeraFarmiga">@VeraFarmiga</a> bot, with words scrambled from her tweets',
 		"image": "/assets/images/verafarmigabot.png",
-		"category": "2"
+		"category": 2
 	},
 	{
 		"id": 9,
@@ -78,7 +78,7 @@ var websites = [
 		"url": "https://vernaflamingo.isamotherfucking.ninja",
 		"description": 'Verna Flamingo is a splash page dedicated to Vera Farmiga. This project is also categorized under <a target="_blank" href="http://th-hq.asclaria.org">Throwaway Projects</a>.',
 		"image": "/assets/images/vernaflamingo.png",
-		"category": "2"
+		"category": 2
 	},
 	{
 		"id": 10,
@@ -86,7 +86,7 @@ var websites = [
 		"url": "https://smoothsailing.asclaria.org",
 		"description": "Smooth Sailing Listings is a web directory or listing for websites of any kind. It is an automated and safe way to list your website!",
 		"image": "/assets/images/smoothsailing.png",
-		"category": "3"
+		"category": 3
 	},
 	{
 		"id": 11,
@@ -94,7 +94,7 @@ var websites = [
 		"url": "https://twitter.com/InnerBuoyancy",
 		"description": "Inner Buoyancy is a positivity bot that tweets reminders a couple of times a day.",
 		"image": "/assets/images/innerbuoyancy.png",
-		"category": "3"
+		"category": 3
 	},
 	{
 		"id": 12,
@@ -102,7 +102,7 @@ var websites = [
 		"url": "https://tests.asclaria.org",
 		"description": "Tests is a subdomain for quizzes and tests for personal or external uses.",
 		"image": "/assets/images/tests.png",
-		"category": "3"
+		"category": 3
 	},
 	{
 		"id": 13,
@@ -110,7 +110,7 @@ var websites = [
 		"url": "https://archives.asclaria.org",
 		"description": "Archives is the place for all things past. It serves as my personal mirror for some favorite things that no longer exist on the internet.",
 		"image": "/assets/images/archives.png",
-		"category": "4"
+		"category": 4
 	},
 	{
 		"id": 14,
@@ -118,7 +118,7 @@ var websites = [
 		"url": "https://vvvvvv.asclaria.org",
 		"description": "VVVVVV is a place of nonsense. Sometimes, I put temporary projects here. Most often, this is where I just mess around.",
 		"image": "/assets/images/vvvvvv.png",
-		"category": "4"
+		"category": 4
 	},
 	{
 		"id": 15,
@@ -126,7 +126,7 @@ var websites = [
 		"url": "https://th-hq.asclaria.org",
 		"description": "Throwaway Headquarters is the headquarters for all of my throwaway websites.",
 		"image": "/assets/images/th-hq.png",
-		"category": "4"
+		"category": 4
 	},
 	{
 		"id": 16,
@@ -134,7 +134,7 @@ var websites = [
 		"url": "https://twitter.com/motelhoscow",
 		"description": "a Twitter bot that tweets at least one animated GIF of Balalaika from Black Lagoon per day",
 		"image": "/assets/images/motelhoscow.png",
-		"category": "2"
+		"category": 2
 	},
 	{
 		"id": 17,
@@ -142,7 +142,7 @@ var websites = [
 		"url": "https://cat.ftp.sh",
 		"description": "Cat FTP lets you store files temporarily for seven (7) days.",
 		"image": "/assets/images/cat.png",
-		"category": "3"
+		"category": 3
 	}
 ];
 
