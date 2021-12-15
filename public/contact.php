ebb4e8b949b9a5f8f5cdbd31c63d2cf8fecb3234
Lysianthus <she@lysianth.us>
2021-12-15 16:40:58 +0800

    :art: Rearrange logic

--- public/contact.php
+++ public/contact.php
@@ -8,6 +8,48 @@ use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\SMTP;
 use PHPMailer\PHPMailer\Exception;
 
+$alert = NULL;
+
+if (isset($_POST['contact'])) {
+	$security = $_POST['security'];
+
+	if ($security == 'rats') {
+		$name = filter_var(stripslashes($_POST['name']), FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+		$email = filter_var($_POST['email'], FILTER_SANITIZE_EMAIL);
+		$url = filter_var($_POST['url'], FILTER_SANITIZE_URL);
+		$subject = filter_var(stripslashes($_POST['subject']), FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+		$message = nl2br(filter_var(stripslashes($_POST['message']), FILTER_SANITIZE_FULL_SPECIAL_CHARS));
+
+		$body = "<p><b>Name:</b> $name</p><p><b>Email Address:</b> $email</p><p><b>Website:</b> $url</p><p><b>Message:</b></p><p>$message</p><hr /><p>This message was sent through the contact form on Asclaria.</p>";
+
+		// PHPMailer
+
+		$mail = new PHPMailer(TRUE);
+
+		try {
+			$mail->isSMTP();
+			$mail->Host = 'mail.asclaria.org';
+			$mail->Port = 25;
+
+			$mail->setFrom('noreply@asclaria.org', 'Asclaria');
+			$mail->addAddress('hello@asclaria.org');
+
+			$mail->isHTML(TRUE);
+			$mail->Subject = "[Asclaria] $subject";
+			$mail->Body = $body;
+
+			$mail->send();
+
+			$alert = "<p>Your message has been sent successfully. Thank you so much for your time. Hopefully, you will receive a reply within the week. Have a great day!</p>";
+
+		} catch (Exception $e) {
+			$alert = "<p>The message could not be sent. Mailer Error: {$mail->ErrorInfo}</p>";
+		}
+	} else {
+		$alert = "<p>Something went wrong. Maybe you’re not human?</p>";
+	}
+}
+
 $title = "Contact | ";
 include 'includes/header.php';
 
@@ -19,56 +61,9 @@ include 'includes/header.php';
 		
 		<h1 class="page__heading">Contact</h1>
 		<article class="article">
-<?php
+			
+			<?php echo $alert; ?>
 
-		if (isset($_POST['contact'])) :
-			$security = $_POST['security'];
-
-			if ($security == 'rats') :
-				$name = filter_var(stripslashes($_POST['name']), FILTER_SANITIZE_FULL_SPECIAL_CHARS);
-				$email = filter_var($_POST['email'], FILTER_SANITIZE_EMAIL);
-				$url = filter_var($_POST['url'], FILTER_SANITIZE_URL);
-				$subject = filter_var(stripslashes($_POST['subject']), FILTER_SANITIZE_FULL_SPECIAL_CHARS);
-				$message = nl2br(filter_var(stripslashes($_POST['message']), FILTER_SANITIZE_FULL_SPECIAL_CHARS));
-
-				$body = "<p><b>Name:</b> $name</p><p><b>Email Address:</b> $email</p><p><b>Website:</b> $url</p><p><b>Message:</b></p><p>$message</p><hr /><p>This message was sent through the contact form on Asclaria.</p>";
-
-				// PHPMailer
-
-				$mail = new PHPMailer(TRUE);
-
-				try {
-					$mail->isSMTP();
-					$mail->Host = 'mail.asclaria.org';
-					$mail->Port = 25;
-
-					$mail->setFrom('noreply@asclaria.org', 'Asclaria');
-					$mail->addAddress('hello@asclaria.org');
-
-					$mail->isHTML(TRUE);
-					$mail->Subject = "[Asclaria] $subject";
-					$mail->Body = $body;
-
-					$mail->send();
-
-				} catch (Exception $e) {
-					echo "<p>The message could not be sent. Mailer Error: {$mail->ErrorInfo}</p>";
-				}
-
-?>
-			<p>Your message has been sent successfully. Thank you so much for your time. Hopefully, you will receive a reply within the week. Have a great day!</p>
-<?php
-
-			else :
-
-?>
-			<p>Something went wrong. Maybe you’re not human?</p>
-<?php
-
-			endif;
-		endif;
-
-?>
 			<p>Use this contact form to send me whatever! If you prefer direct email, you can send me your love at <a href="mailto:hello@asclaria.org">hello@asclaria.org</a>.</p>
 			<p>Marked (*) fields are required.</p>
 			<form action="#" method="post">
