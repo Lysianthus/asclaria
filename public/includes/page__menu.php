cd9b9d31b6fc05682237fc570e4fa9e042da3698
Lysianthus <she@lysianth.us>
2024-12-10 02:35:36 +0800

    :fire: Remove Twitter link

--- public/includes/page__menu.php
+++ public/includes/page__menu.php
@@ -18,6 +18,5 @@
 				</li>
 				<li><a href="/linkage/">/linkage</a></li>
 				<li><a href="/contact/">/contact</a></li>
-				<li><a target="_blank" href="https://twitter.com/asclaria">Twitter</a></li>
 			</ul> <!-- .page__menu__list -->
 		</div> <!-- .page__menu -->
