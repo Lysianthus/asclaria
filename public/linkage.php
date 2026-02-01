3cc6a663a942961dded912dd2c004c55c8ae874a
Lysianthus <she@lysianth.us>
2026-02-02 01:15:32 +0800

    :art: Update linkage.php

--- public/linkage.php
+++ public/linkage.php
@@ -13,72 +13,49 @@ include 'includes/header.php';
 		<article class="article">
 			<h2>Admired</h2>
 			<p>Here is a list of collectives that I admire! These networks give me so much inspiration. Do check them out!</p>
-			<p style="display: flex; justify-content: center; align-items: flex-start; flex-wrap: wrap;">
-				<a target="_blank" href="http://aminuet.com" style="margin: 5px;"><img alt="" src="/assets/images/aminuet.gif" /></a>
-				<a target="_blank" href="http://azurelight.net" style="margin: 5px;"><img alt="" src="/assets/images/azurelight.gif" /></a>
-				<a target="_blank" href="http://caelestis.nu" style="margin: 5px;"><img alt="" src="/assets/images/caelestis.png" /></a>
-				<a target="_blank" href="http://eternal-wings.net" style="margin: 5px;"><img alt="" src="/assets/images/eternal-wings.png" /></a>
-				<a target="_blank" href="http://ishiryoku.net" style="margin: 5px;"><img alt="" src="/assets/images/ishiryoku.png?v=1" /></a>
-				<a target="_blank" href="http://kyou.nu" style="margin: 5px;"><img alt="" src="/assets/images/kyou.jpg" /></a>
-				<a target="_blank" href="http://like-knives.org" style="margin: 5px;"><img alt="" src="/assets/images/like-knives.png" /></a>
-				<a target="_blank" href="http://londonboy.org" style="margin: 5px;"><img alt="" src="/assets/images/londonboy.png" /></a>
-				<a target="_blank" href="http://midnight-cloud.net" style="margin: 5px;"><img alt="" src="/assets/images/midnight-cloud.jpg" /></a>
-				<a target="_blank" href="http://nightbringer.net" style="margin: 5px;"><img alt="" src="/assets/images/nightbringer.gif" /></a>
-				<a target="_blank" href="http://northstar.nu" style="margin: 5px;"><img alt="" src="/assets/images/northstar.png" /></a>
-				<a target="_blank" href="http://ohmydarling.org" style="margin: 5px;"><img alt="" src="/assets/images/ohmydarling.jpg" /></a>
-				<a target="_blank" href="http://overskill.org" style="margin: 5px;"><img alt="" src="/assets/images/overskill.png" /></a>
-				<a target="_blank" href="http://piratesboard.net" style="margin: 5px;"><img alt="" src="/assets/images/piratesboard.png" /></a>
-				<a target="_blank" href="http://potion.nu" style="margin: 5px;"><img alt="" src="/assets/images/potion.png" /></a>
-				<a target="_blank" href="http://www.psyche.nu" style="margin: 5px;"><img alt="" src="/assets/images/psyche.jpg" /></a>
-				<a target="_blank" href="http://quicksilver.nu" style="margin: 5px;"><img alt="" src="/assets/images/quicksilver.png" /></a>
-				<a target="_blank" href="http://raygunsue.org" style="margin: 5px;"><img alt="" src="/assets/images/raygunsue.png" /></a>
-				<a target="_blank" href="http://redcrown.net" style="margin: 5px;"><img alt="" src="/assets/images/redcrown.png?v=1" /></a>
-				<a target="_blank" href="http://rydia.nu" style="margin: 5px;"><img alt="" src="/assets/images/rydia.png" /></a>
-				<a target="_blank" href="http://shinkirou.net" style="margin: 5px;"><img alt="" src="/assets/images/shinkirou.png" /></a>
-				<a target="_blank" href="http://shinshoku.net" style="margin: 5px;"><img alt="" src="/assets/images/shinshoku.png" /></a>
-				<a target="_blank" href="http://skycrown.org" style="margin: 5px;"><img alt="" src="/assets/images/skycrown.png" /></a>
-				<a target="_blank" href="http://snow-heart.net" style="margin: 5px;"><img alt="" src="/assets/images/snowheart.jpg" /></a>
-				<a target="_blank" href="http://somnus.nu" style="margin: 5px;"><img alt="" src="/assets/images/somnus.png" /></a>
-				<a target="_blank" href="http://www.sun-cryst.org" style="margin: 5px;"><img alt="" src="/assets/images/sun-cryst.gif" /></a>
-				<a target="_blank" href="http://sutarodo.net" style="margin: 5px;"><img alt="" src="/assets/images/sutarodo.png" /></a>
-				<a target="_blank" href="http://valiantknife.org" style="margin: 5px;"><img alt="" src="/assets/images/valiantknife.gif" /></a>
-				<a target="_blank" href="http://www.wild-seven.org" style="margin: 5px;"><img alt="" src="/assets/images/wild-seven.gif" /></a>
-				<a target="_blank" href="http://withinmyworld.org" style="margin: 5px;"><img alt="" src="/assets/images/withinmyworld.png" /></a>
-				<a target="_blank" href="http://zabimaru.net" style="margin: 5px;"><img alt="" src="/assets/images/zabimaru.gif" /></a>
-				<a target="_blank" href="http://zeruda.org" style="margin: 5px;"><img alt="" src="/assets/images/zeruda.gif?v=1" /></a>
-			</p>
-			<p style="text-align: center">
+			<div style="display: flex; justify-content: center; align-items: center; flex-wrap: wrap;">
+				<a target="_blank" href="https://caelestis.nu" style="margin: 5px;"><img alt="" src="/assets/images/caelestis.png" /></a>
+				<a target="_blank" href="https://eternal-wings.net" style="margin: 5px;"><img alt="" src="/assets/images/eternal-wings.png" /></a>
+				<a target="_blank" href="https://ishiryoku.net" style="margin: 5px;"><img alt="" src="/assets/images/ishiryoku.png?v=1" /></a>
+				<a target="_blank" href="https://kyou.nu" style="margin: 5px;"><img alt="" src="/assets/images/kyou.jpg" /></a>
+				<a target="_blank" href="https://like-knives.org" style="margin: 5px;"><img alt="" src="/assets/images/like-knives.png" /></a>
+				<a target="_blank" href="https://lost-boy.org" style="margin: 5px;"><img alt="" src="/assets/images/lost-boy.png" /></a>
+				<a target="_blank" href="https://midnight-cloud.net" style="margin: 5px;"><img alt="" src="/assets/images/midnight-cloud.jpg" /></a>
+				<a target="_blank" href="https://ohmydarling.org" style="margin: 5px;"><img alt="" src="/assets/images/ohmydarling.jpg" /></a>
+				<a target="_blank" href="https://www.psyche.nu" style="margin: 5px;"><img alt="" src="/assets/images/psyche.jpg" /></a>
+				<a target="_blank" href="https://quicksilver.nu" style="margin: 5px;"><img alt="" src="/assets/images/quicksilver.png" /></a>
+				<a target="_blank" href="https://redcrown.net" style="margin: 5px;"><img alt="" src="/assets/images/redcrown.png?v=1" /></a>
+				<a target="_blank" href="https://shinshoku.net" style="margin: 5px;"><img alt="" src="/assets/images/shinshoku.png" /></a>
+				<a target="_blank" href="https://skycrown.org" style="margin: 5px;"><img alt="" src="/assets/images/skycrown.png" /></a>
+				<a target="_blank" href="https://snow-heart.net" style="margin: 5px;"><img alt="" src="/assets/images/snowheart.jpg" /></a>
+				<a target="_blank" href="https://somnus.nu" style="margin: 5px;"><img alt="" src="/assets/images/somnus.png" /></a>
+				<a target="_blank" href="https://www.wild-seven.org" style="margin: 5px;"><img alt="" src="/assets/images/wild-seven.gif" /></a>
+				<a target="_blank" href="https://withinmyworld.org" style="margin: 5px;"><img alt="" src="/assets/images/withinmyworld.png" /></a>
 				<a target="_blank" href="https://love.suga.nu"><img alt="" src="/assets/images/amour.png" /></a>
-			</p>
-			<p style="display: flex; justify-content: center; align-items: flex-start; flex-wrap: wrap;">
-				<a target="_blank" href="http://ontheaxis.net" style="margin: 5px;"><img alt="" src="/assets/images/ontheaxis.png" /></a>
-				<a target="_blank" href="http://oubliette.nu" style="margin: 5px;"><img alt="" src="/assets/images/oubliette.png" /></a>
-			</p>
-			<p style="display: flex; justify-content: space-around; align-items: flex-start; flex-wrap: wrap;">
-				<a target="_blank" href="http://aroceu.com">Aroceu</a>
-				<a target="_blank" href="http://erzadel.net">Erzadel</a>
-				<a target="_blank" href="http://lenne.nu">Lenne</a>
-				<a target="_blank" href="http://rigelatin.net">Rigelatin</a>
-				<a target="_blank" href="http://sohryu.nu">Sohryu</a>
-			</p>
+				<a target="_blank" href="https://ontheaxis.net" style="margin: 5px;"><img alt="" src="/assets/images/ontheaxis.png" /></a>
+				<a target="_blank" href="https://oubliette.nu" style="margin: 5px;"><img alt="" src="/assets/images/oubliette.png" /></a>
+				<a target="_blank" href="https://rigelatin.net" style="margin: 5px;"><img alt="" src="/assets/images/rigelatin.gif" /></a>
+				<a target="_blank" href="https://kingdra.net" style="margin: 5px;"><img alt="" src="/assets/images/kingdra.png" /></a>
+				<!-- <a target="_blank" href="https://sohryu.nu">Sohryu</a> -->
+			</div>
 			<h2>Listed At</h2>
 			<p style="display: flex; justify-content: center; align-items: flex-start; flex-wrap: wrap;">
 				<a target="_blank" href="https://smoothsailing.asclaria.org" style="margin: 5px;"><img alt="" src="/umbrella/smoothsailing/assets/images/microbutton.png"></a>
-				<a target="_blank" href="http://yummy.nu" style="margin: 5px;"><img alt="" src="/assets/images/yummy.png"></a>
+				<a target="_blank" href="https://yummy.nu" style="margin: 5px;"><img alt="" src="/assets/images/yummy.png"></a>
 			</p>
 			<h2>Claims</h2>
-			<p style="text-align: center;">Asclaria is guarded by <a target="_blank" href="http://samurai.genkigirl.net"><img alt="Sage of Halo" src="/assets/images/samurai.gif" />, the Sage of Halo</a>.</p>
-			<p style="text-align: center;">Asclaria is under the protection of <a target="_blank" href="http://patron.snow-heart.net">Haliya</a>.</p>
-			<p style="text-align: center;"><a target="_blank" href="http://ohmydarling.org/food/">just can't get enough</a> » Lasagna, baby!</p>
-			<p style="text-align: center;">I'm a <a target="_blank" href="http://pkmn.zeruda.org/clique/"><img alt="" src="/assets/images/water.gif">/<img alt="" src="/assets/images/fairy.gif" /></a> type!</p>
+			<p style="text-align: center;">Asclaria is guarded by <a target="_blank" href="https://samurai.genkigirl.net"><img alt="Sage of Halo" src="/assets/images/samurai.gif" />, the Sage of Halo</a>.</p>
+			<p style="text-align: center;">Asclaria is under the protection of <a target="_blank" href="https://patron.snow-heart.net">Haliya</a>.</p>
+			<p style="text-align: center;"><a target="_blank" href="https://ohmydarling.org/food/">just can't get enough</a> » Lasagna, baby!</p>
+			<p style="text-align: center;">I'm a <a target="_blank" href="https://pkmn.zeruda.org/clique/"><img alt="" src="/assets/images/water.gif">/<img alt="" src="/assets/images/fairy.gif" /></a> type!</p>
 			<h2>Joined Cliques & Webrings</h2>
 			<p style="display: flex; justify-content: center; align-items: center; flex-wrap: wrap;">
-				<a target="_blank" href="http://patron.snow-heart.net" style="margin: 5px;"><img alt="" src="/assets/images/patron.gif" /></a>
-				<a target="_blank" href="http://oceanflare.net/stargazer/" style="margin: 5px;"><img alt="" src="/assets/images/stargazer.gif" /></a>
-				<a target="_blank" href="http://ohmydarling.org/food/" style="margin: 5px;"><img alt="" src="/assets/images/food.png" /></a>
-				<a target="_blank" href="http://cliques.ruby-wings.net/fight/" style="margin: 5px;"><img alt="" src="/assets/images/fight.png" /></a>
-				<a target="_blank" href="http://pkmn.zeruda.org/clique/" style="margin: 5px;"><img alt="" src="/assets/images/pokemon.gif" /></a>
-				<a target="_blank" href="http://samurai.genkigirl.net" style="margin: 5px;"><img alt="" src="/assets/images/ronin.png" /></a>
+				<a target="_blank" href="https://patron.snow-heart.net" style="margin: 5px;"><img alt="" src="/assets/images/patron.gif" /></a>
+				<a target="_blank" href="https://oceanflare.net/stargazer/" style="margin: 5px;"><img alt="" src="/assets/images/stargazer.gif" /></a>
+				<a target="_blank" href="https://ohmydarling.org/food/" style="margin: 5px;"><img alt="" src="/assets/images/food.png" /></a>
+				<a target="_blank" href="https://cliques.ruby-wings.net/fight/" style="margin: 5px;"><img alt="" src="/assets/images/fight.png" /></a>
+				<a target="_blank" href="https://pkmn.zeruda.org/clique/" style="margin: 5px;"><img alt="" src="/assets/images/pokemon.gif" /></a>
+				<a target="_blank" href="https://samurai.genkigirl.net" style="margin: 5px;"><img alt="" src="/assets/images/ronin.png" /></a>
 			</p>
 			<p style="text-align: center;">
 
@@ -103,7 +80,7 @@ include 'includes/header.php';
 				<img alt="Asclaria 88x31" src="/assets/images/asclaria_88x31_2.png" />
 				<img alt="Asclaria 88x31" src="/assets/images/asclaria_88x31_1.png" />
 			</p>
-			<p>Thank you!</p>
+			<p>Thank you! ⭐</p>
 		</article> <!-- .article -->
 		
 	</div> <!-- .page -->
